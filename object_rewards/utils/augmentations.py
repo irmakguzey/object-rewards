604d2a597d3aa46f76b0bf86912a57b63d616815
irmakguzey <irmakguzey47@gmail.com>
2024-11-11 18:04:39 -0500

    Removed some comments from augmentation utils

--- object_rewards/utils/augmentations.py
+++ object_rewards/utils/augmentations.py
@@ -8,9 +8,5 @@ def crop_transform(
         return crop(image, 0, 80, image_size, image_size)
     if camera_view == 1:
         return crop(image, 0, 80, image_size, image_size)
-    # elif camera_view == 1: # NOTE: For now we don't have any other views
-    #     return crop(image, 0,90, image_size,image_size)
-    elif camera_view == 2:
-        return crop(image, 0, 40, image_size, image_size)
 
     print(f"crop_transform returning None for camera_view={camera_view}")
